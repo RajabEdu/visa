33ad32b58e0d9edb1fd9177024f34affc6e93034
Rajab Edu <edwardrajab@gmail.com>
2026-06-10 03:47:36 -0500

    fix: use socks5 proxy

--- dist/workflow.js
+++ dist/workflow.js
@@ -86,7 +86,7 @@ class Workflow extends retryable_execution_1.default {
     buildBrowserOptions() {
         if (process.env.BROWSERLESS_API_KEY) {
             return {
-                browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_API_KEY}&stealth=true&--proxy-server=${process.env.PROXY_SERVER}`,
+                browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_API_KEY}&stealth=true&--proxy-server=socks5://${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@${process.env.PROXY_SERVER}`,
             };
         }
         return {
